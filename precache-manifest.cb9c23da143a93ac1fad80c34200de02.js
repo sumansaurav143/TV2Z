68e1b817d6ac0ee5f368acc1cc10eebd8944104f
sumansaurav143 <sumansaurav8294@gmail.com>
2022-04-17 17:04:03 +0530

    Updates

---
+++ precache-manifest.cb9c23da143a93ac1fad80c34200de02.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "005465bcaa60a557c68d",
+    "url": "/TV2Z/static/css/main.337dc0cc.chunk.css"
+  },
+  {
+    "revision": "005465bcaa60a557c68d",
+    "url": "/TV2Z/static/js/main.005465bc.chunk.js"
+  },
+  {
+    "revision": "7b2f6e4430968a13bf15",
+    "url": "/TV2Z/static/js/1.7b2f6e44.chunk.js"
+  },
+  {
+    "revision": "989c63769fb254989aa7",
+    "url": "/TV2Z/static/js/runtime~main.989c6376.js"
+  },
+  {
+    "revision": "00a18db19e8cf233e0c0c60d25c1c454",
+    "url": "/TV2Z/static/media/19-02.00a18db1.jpeg"
+  },
+  {
+    "revision": "268ea240d717bfb8d38c89282a972815",
+    "url": "/TV2Z/static/media/1911 Revolution.268ea240.jpeg"
+  },
+  {
+    "revision": "18ef67206b41592336e08b5f2d1f1d2f",
+    "url": "/TV2Z/static/media/Modus.18ef6720.jpeg"
+  },
+  {
+    "revision": "0d268b9700a98f452512df0743432c04",
+    "url": "/TV2Z/static/media/Einstein.0d268b97.jpeg"
+  },
+  {
+    "revision": "626eb7f3d9ccbb9bbdce8a442a5afeda",
+    "url": "/TV2Z/static/media/Medici.626eb7f3.jpeg"
+  },
+  {
+    "revision": "666c93426e0c06ba702085acd3d5f356",
+    "url": "/TV2Z/static/media/Picnic.666c9342.jpeg"
+  },
+  {
+    "revision": "774c7555b1ea05221271077563ef7aab",
+    "url": "/TV2Z/static/media/Wolf Creek.774c7555.jpeg"
+  },
+  {
+    "revision": "4e3612e8ffe0d2b911be4f7a470fdfac",
+    "url": "/TV2Z/static/media/logo.4e3612e8.svg"
+  },
+  {
+    "revision": "d9043e920166e4c29805fcd6c3b3c1a9",
+    "url": "/TV2Z/static/media/Eye-on.d9043e92.svg"
+  },
+  {
+    "revision": "760a9ef3abd40d179dee5571e498a189",
+    "url": "/TV2Z/static/media/Eye-off.760a9ef3.svg"
+  },
+  {
+    "revision": "e20261a6ea5ff1b3a06ae2876e414ec1",
+    "url": "/TV2Z/static/media/Christmas Story.e20261a6.jpeg"
+  },
+  {
+    "revision": "90b27c946c0db26051ec5a296b4e143a",
+    "url": "/TV2Z/static/media/Fighter.90b27c94.jpeg"
+  },
+  {
+    "revision": "020050b67aa04afb452d396fd76ff4c8",
+    "url": "/TV2Z/static/media/Hells gate.020050b6.jpeg"
+  },
+  {
+    "revision": "622941f87d6b2251ade28a038704cbda",
+    "url": "/TV2Z/static/media/Dropdown.622941f8.svg"
+  },
+  {
+    "revision": "de101155315e5d9410f7c6adb2e830b1",
+    "url": "/TV2Z/static/media/Search.de101155.svg"
+  },
+  {
+    "revision": "6de74d052290ec5d015d62963b5d1d18",
+    "url": "/TV2Z/index.html"
+  }
+];
